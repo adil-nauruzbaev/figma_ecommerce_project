afe20db170538b4bfc16d14ed464fdb7c5bf6d65
adil-nauruzbaev <aresbitch@mail.ru>
2022-12-10 22:56:11 +0600

    GeolocationWidget

---
+++ lib/features/home/presentation/widgets/Geolocation/geolocation_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/common/app_colors/app_colors.dart';
+import 'package:flutter_ecommerce/common/app_custom_icons.dart/svg_icons.dart';
+
+class GeolocationWidget extends StatelessWidget {
+  const GeolocationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                backgroundColor: Colors.grey[50],
+                elevation: 0,
+              ),
+              child: svgGeolocation,
+            ),
+            const Text(
+              'Astana, KZ',
+              style: TextStyle(
+                fontFamily: 'MarkPronormal400',
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: AppColors.buttonBarColor,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                backgroundColor: Colors.grey[50],
+                elevation: 0,
+              ),
+              child: svgDown,
+            ),
+          ],
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            backgroundColor: Colors.grey[50],
+            elevation: 0,
+          ),
+          child: svgFilter,
+        ),
+      ],
+    );
+  }
+}
