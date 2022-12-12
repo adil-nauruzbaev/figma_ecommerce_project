1fa61eafdcab4da7ae1b24ba70ca3f79994c330f
adil-nauruzbaev <aresbitch@mail.ru>
2022-12-12 19:37:28 +0600

    Models for Cart

---
+++ lib/features/cart/data/models/cart_model.dart
@@ -0,0 +1,23 @@
+import 'package:figma_ecommerce_project/features/cart/data/models/basket_model.dart';
+import 'package:figma_ecommerce_project/features/cart/domain/entities/cart_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cart_model.g.dart';
+
+@JsonSerializable()
+class CartModel extends CartEntity{
+  const CartModel({
+    required List<BasketModel>? basket,
+    required String? delivery, 
+    required String? id,
+    required int? total,
+  }) : super(
+    id: id,
+    delivery: delivery,
+    total: total,
+    basket: basket,
+  );
+
+  factory CartModel.fromJson(Map<String, dynamic> json) => _$CartModelFromJson(json);
+  Map<String, dynamic> toJson() => _$CartModelToJson(this);
+}
