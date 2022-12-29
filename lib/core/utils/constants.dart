1150d1e94099ea00deab4444513003bdc5853f4e
adil-nauruzbaev <aresbitch@mail.ru>
2022-12-29 20:43:22 +0600

    Поменял мок, т.к ссылка на одну картинку выдавала http 502

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,4 @@
-const String homeUrl = 'https://run.mocky.io/v3/654bd15e-b121-49ba-a588-960956b15175';
+const String homeUrl = 'https://run.mocky.io/v3/f2cba9d1-4e42-42a2-bd46-91e8f898ae38';
 const String cartUrl = 'https://run.mocky.io/v3/53539a72-3c5f-4f30-bbb1-6ca10d42c149';
 const String productUrl = 'https://run.mocky.io/v3/6c14c560-15c6-4248-b9d2-b4508df7d4f5';
 
